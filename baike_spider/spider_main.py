1f8a00502ee6c39d7826bf8fd3879cf1f9786c82
zision <zhouzijian1996@gmail.com>
2018-01-07 22:34:24 +0800

    7-2 调度程序
    修改完善了一些细节

--- baike_spider/spider_main.py
+++ baike_spider/spider_main.py
@@ -15,24 +15,24 @@ class SpiderMain(object):
             try:
                 new_url = self.urls.get_new_url()
                 print('craw %d : %s' % (count, new_url))
-                html_cont = self.downloader.download()
+                html_cont = self.downloader.download(new_url)
                 new_urls, new_data = self.parser.parse(new_url, html_cont)
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
 
-                if count >= 1000:
+                if count >= 100:
                     break
 
                 count += 1
 
-            except:
-                print('craw failed')
+            except Exception as e:
+                print(str(e))
+                # 根据报错信息提示错误
 
-
-        self.outputer.output_html()
+            self.outputer.output_html()
 
 
 if __name__ == '__main__':
-    root_url = 'https://baike.baidu.com/item/Python/407313'
+    root_url = 'https://baike.baidu.com/item/Python/407313?fr=aladdin'
     obj_spider = SpiderMain()
     obj_spider.craw(root_url)
