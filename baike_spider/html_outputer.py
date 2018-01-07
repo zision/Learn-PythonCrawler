0364ffa1a6bed6f7d6c1761a3368be838800b2f3
zision <zhouzijian1996@gmail.com>
2018-01-07 22:35:45 +0800

    7-6 HTML输出器
    修改完善了一些细节

--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -3,25 +3,27 @@ class HtmlOutputer(object):
         self.datas = []
 
     def collect_data(self, data):
-        if data == None:
+        if data is None:
             return
         self.datas.append(data)
 
     def output_html(self):
-        fout = open('output.html', 'w',encoding='utf-8')
-
+        fout = open('output.html', 'w', encoding='utf-8')
         fout.write("<html>")
         fout.write("<body>")
-        fout.write("<table>")
+        fout.write("<a>")
 
         for data in self.datas:
-            fout.write("<tr>")
-            fout.write("<td>%s</td>" % data['url'])
-            fout.write("<td>%s</td>" % data['title'])
-            fout.write("<td>%s</td>" % data['summary'])
-            fout.write("</tr>")
+            # 屏蔽掉原来的，重新写一个更美观的输出效果
+            # fout.write("<tr>")
+            # fout.write("<td>%s</td>" % data['url'])
+            # fout.write("<td>%s</td>" % data['title'])
+            # fout.write("<td>%s</td>" % data['summary'])
+            # fout.write("</tr>")
+            fout.write('<a href="%s">%s</a>' % (data['url'], data['title']))
+            fout.write('<p>%s</p>' % data['summary'])
 
-        fout.write("</table>")
+        fout.write("</a>")
         fout.write("</body>")
         fout.write("</html>")
 
