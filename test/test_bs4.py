bbb9f86e3ca9d5af80d5d285f01c665c6b3597b9
zision <zhouzijian1996@gmail.com>
2018-01-07 17:52:31 +0800

    6-4 BeautifulSoup实例测试
    这里需要注意Py3中不需要输入from_encoding=‘utf-8’。python3缺省的编码是unicode, 再在from_encoding设置为utf8, 会被忽视掉, 不去掉会报错。

--- test/test_bs4.py
+++ test/test_bs4.py
@@ -1,4 +1,6 @@
+import re
 from bs4 import BeautifulSoup
+
 '''
 # 这里是6-3 BeautifulSoup的语法 的笔记内容。可以通过删除三引号取消屏蔽状态
 import re
@@ -33,3 +35,47 @@ soup.find_all('div', class_='abc', string='Python')
 # 获取查找到的a节点的链接文字
 # node.get_text()
 '''
+
+
+# 6-4 BeautifulSoup实例测试
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+# python3 缺省的编码是unicode, 再在from_encoding设置为utf8, 会被忽视掉, 故去掉，否则会报错
+
+# 第一种方法
+print('获取所有的链接')
+links = soup.find_all('a')
+for link in links:
+    print(link.name, link['href'], link.get_text())
+
+
+# 第二种方法
+print('获取lacie的链接')
+link_node = soup.find('a', href='http://example.com/lacie')
+print(link_node.name, link_node['href'], link_node.get_text())
+
+
+# 第三种方法
+print('正则匹配')
+link_node = soup.find('a', href=re.compile(r'ill'))
+print(link_node.name, link_node['href'], link_node.get_text())
+
+
+# 第四种方法
+print('获取p段落文字')
+p_node = soup.find('p', class_='title')
+print(p_node.name, p_node.get_text())
