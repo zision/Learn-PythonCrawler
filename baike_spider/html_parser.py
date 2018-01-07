9e9326d9e022e4d75a840392a8cc55be2c863a93
zision <zhouzijian1996@gmail.com>
2018-01-07 19:57:15 +0800

    7-5 HTML解析器html_parser
    针对py3有细微改动

--- baike_spider/html_parser.py
+++ baike_spider/html_parser.py
@@ -1,3 +1,41 @@
+import re
+import urllib.parse
+from bs4 import BeautifulSoup
+
+
 class HtmlParser(object):
-    def parse(self, new_url, html_cont):
-        pass
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        links = soup.find_all('a', href=re.compile(r'/item/(.*)'))
+        # 网址有变，表达式做了调整
+        for link in links:
+            new_url = link['href']
+            new_full_url = urllib.parse.urljoin(page_url, new_url)
+            # Py3中用到的模块名称变为urllib.parse
+            new_urls.add(new_full_url)
+        return new_urls
+
+    def _get_new_data(self, page_url, soup):
+        res_data = {}
+
+        # url
+        res_data['url'] = page_url
+
+        # <dd class="lemmaWgt-lemmaTitle-title"> <h1>Python</h1>
+        title_node = soup.find('dd', class_='lemmaWgt-lemmaTitle-title"').find('h1')
+        res_data['title'] = title_node.get_text()
+
+        # <div class="lemma-summary" label-module="lemmaSummary">
+        summary_nod = soup.find('div', class_='lemma-summary')
+        res_data['summary'] = summary_nod.get_text()
+
+        return res_data
+
+    def parse(self, page_url, html_cont):
+        if page_url is None or html_cont is None:
+            return
+
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_data = self._get_new_data(page_url, soup)
+        return new_urls, new_data
