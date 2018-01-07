45d7bf644da119254842700361b6c37f007181a1
zision <zhouzijian1996@gmail.com>
2018-01-07 19:26:49 +0800

    7-4 HTML下载器html_downloader

--- baike_spider/html_downloader.py
+++ baike_spider/html_downloader.py
@@ -1,3 +1,14 @@
+from urllib import request
+
+
 class HtmlDownloader(object):
-    def download(self):
-        pass
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = request.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
