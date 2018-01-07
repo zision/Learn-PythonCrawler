c86f6590af2376484bccaeefc72514f9187eae37
zision <zhouzijian1996@gmail.com>
2018-01-07 20:45:54 +0800

    7-6 HTML输出器

--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -1,6 +1,28 @@
 class HtmlOutputer(object):
-    def collect_data(self, new_data):
-        pass
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data == None:
+            return
+        self.datas.append(data)
 
     def output_html(self):
-        pass
+        fout = open('output.html', 'w',encoding='utf-8')
+
+        fout.write("<html>")
+        fout.write("<body>")
+        fout.write("<table>")
+
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'])
+            fout.write("<td>%s</td>" % data['summary'])
+            fout.write("</tr>")
+
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
+
+        fout.close()
